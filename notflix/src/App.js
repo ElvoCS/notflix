c3606284e5ee8e4d5bc0c3e467ead27b5d51c352
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-14 11:20:40 +0100

    setting up rows + movie array

--- notflix/src/App.js
+++ notflix/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Notflix</h1>
+      <Row title = "NETFLIX ORIGINALS"/>
+      <Row title = "TRENDING NOW"/>
+
     </div>
   );
 }
---
+++ notflix/src/Row.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function Row({ title }) {
+    const [movies, setMovies] = useState([]);
+
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            {/*container -> poster */}
+
+        </div>
+    )
+}
+
+export default Row
