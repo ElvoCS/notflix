7e57ad0fa7ecdf318da1ed9e483927487572cc00
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-14 15:01:39 +0100

    displaying array of movies, doing bitta styling

--- notflix/src/Row.js
+++ notflix/src/Row.js
@@ -1,28 +1,42 @@
-import React, { useState, useEffect, fetchUrl } from 'react'
-import axios from './axios';
+import React, { useState, useEffect, fetchUrl } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() =>{
-        //if brackets are blank run once when row loads and don't run it again
-        //make an async call because we're making a request from an outside source
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            return request;
-        }
-        fetchData()
-    }, []);
+  useEffect(() => {
+    //if brackets are blank run once when row loads and don't run it again
+    //make an async call because we're making a request from an outside source
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    return (
-        <div>
-            <h2>{title}</h2>
+  console.log(movies);
 
-            {/*container -> poster */}
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-        </div>
-    )
+      <div className="row_posters">
+        {/*several row_posters*/}
+
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
