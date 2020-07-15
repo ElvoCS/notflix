f4016444096a37dbe29c06a5b168c7d3b8eba176
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-15 10:09:51 +0100

    adding gradient; banner is finished

--- notflix/src/Banner.js
+++ notflix/src/Banner.js
@@ -45,6 +45,8 @@ function Banner() {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+
+      <div className="banner-fadeBottom"></div>
     </header>
   );
 }
